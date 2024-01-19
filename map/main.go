8a9aef74772d161af68ceba4e54f738a1228acc4
EduardoZimelewicz <eduardo.zimelewicz@gmail.com>
2024-01-19 15:22:26 -0300

    maps

--- map/main.go
+++ map/main.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	colors := map[string]string{
 		"red":   "#ff0000",
-		"green": "#2342f",
+		"green": "#4bf745",
+		"white": "#ffffff",
 	}
 
-	fmt.Println(colors)
+	printMap(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
